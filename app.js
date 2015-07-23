c67bd7eb89ab1624e7e725c86e4abcd34dccd83c
Jose Muñoz <jose.coletii@gmail.com>
2015-07-23 19:47:08 +0200

    Primera pregunta

--- app.js
+++ app.js
@@ -24,8 +24,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// RUTAS
 app.use('/', routes);
 
+// ERRORES
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
