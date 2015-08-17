6dab9e10c9ff4fb5cc70afa86a5ac248bfeb4b0c
Jose María Muñoz <jmunozkr@gmail.com>
2015-08-17 22:37:16 +0200

    Creación de comentarios

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 // controllers
 
 var quizController = require ('../controllers/quiz_controller');
+var commentController = require('../controllers/comment_controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -22,6 +23,9 @@ router.post('/quizes/create',              quizController.create);
 router.get('/quizes/:quizId(\\d+)/edit',   quizController.edit);
 router.put('/quizes/:quizId(\\d+)',        quizController.update);
 router.delete('/quizes/:quizId(\\d+)',     quizController.destroy);
+
+router.get('/quizes/:quizId(\\d+)/comments/new',            commentController.new);
+router.post('/quizes/:quizId(\\d+)/comments',              commentController.create);
 
 // Ampliación para admitir a los autores
 router.get('/author', function(req, res, next){
