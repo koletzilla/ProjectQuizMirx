9799133d610b699a49773c605637148bc21aad32
Jose María Muñoz <jmunozkr@gmail.com>
2015-08-17 21:17:05 +0200

    Borrar pregunta

--- routes/index.js
+++ routes/index.js
@@ -21,6 +21,7 @@ router.get('/quizes/new',                  quizController.new);
 router.post('/quizes/create',              quizController.create);
 router.get('/quizes/:quizId(\\d+)/edit',   quizController.edit);
 router.put('/quizes/:quizId(\\d+)',        quizController.update);
+router.delete('/quizes/:quizId(\\d+)',     quizController.destroy);
 
 // Ampliación para admitir a los autores
 router.get('/author', function(req, res, next){
