d2a7630fd45111908e3233a25dab20f55532dc52
Jose María Muñoz <jmunozkr@gmail.com>
2015-08-17 17:20:32 +0200

    Adaptación de models.js a Heroku

--- models/models.js
+++ models/models.js
@@ -15,10 +15,16 @@ var storage  = process.env.DATABASE_STORAGE;
 // Cargar Modelo ORM
 var Sequelize = require('sequelize');
 
-// Usar BBDD SQLite:
-var sequelize = new Sequelize(null, null, null,
-                       {dialect: "sqlite", storage: "quiz.sqlite"}
-                    );
+// Usar BBDD SQLite o Postgres
+var sequelize = new Sequelize(DB_name, user, pwd,
+  { dialect:  protocol,
+    protocol: protocol,
+    port:     port,
+    host:     host,
+    storage:  storage,  // solo SQLite (.env)
+    omitNull: true      // solo Postgres
+  }
+);
 
 // Importar la definicion de la tabla Quiz en quiz.js
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
